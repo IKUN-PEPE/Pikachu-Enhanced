c9cea65c9282993929d1159f43cfec2f8ca050b1
Pikachu Developer <dev@pikachu.local>
2026-08-05 17:46:18 +0800

    refactor: 整理项目根目录结构，清除冗余测试脚本与废弃文件，归档设计文档至 docs/ 目录

--- test_phar.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Logger { 
-    public function __destruct() { 
-        echo "DESTRUCT CALLED!\n"; 
-    } 
-} 
-file_exists("phar:///var/www/html/vul/phar/evil_payload.jpg");
--- test_phar2.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Logger { 
-    public function __destruct() { 
-        echo "DESTRUCT CALLED!\n"; 
-    } 
-} 
-$p = new Phar("/var/www/html/vul/phar/evil_payload.jpg");
-$m = $p->getMetadata();
